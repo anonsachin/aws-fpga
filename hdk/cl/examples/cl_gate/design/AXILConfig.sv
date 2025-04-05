4af5f081bc55934273c14efb3696eab3de749e9c
anonsachin <sachin94roxz@gmail.com>
2025-04-05 17:50:59 +0530

    Changed the read mask logic.

--- hdk/cl/examples/cl_gate/design/AXILConfig.sv
+++ hdk/cl/examples/cl_gate/design/AXILConfig.sv
@@ -62,7 +62,7 @@ end
 // writing operands
 ///////////////////////////////////////////////////////////////////////////
 logic [$clog2(CONFIG_SIZE + 2) -1:0] write_addr;
-logic mask_enable;
+logic mask_disable;
 assign axil_awready = 1;
 
 always_ff @ (posedge clk)
@@ -71,13 +71,13 @@ if(resetn)
 begin
     if(axil_awvalid)
     begin
-        mask_enable <= axil_awaddr > START_OFFSET;
+        mask_disable <= ~(axil_awaddr >= START_OFFSET & (axil_awaddr <= (START_OFFSET + ((CONFIG_SIZE + 2) <<2))));
         write_addr <= axil_awaddr[2+:$clog2(CONFIG_SIZE + 2)];
     end
 end
 else
 begin
-  mask_enable <= 0;
+  mask_disable <= 0;
   write_addr <= 0;
 end
 end
@@ -90,7 +90,7 @@ always_ff @ (posedge clk)
 begin
 if(resetn)
 begin
-  if (axil_wvalid & (~mask_enable))
+  if (axil_wvalid & (~mask_disable))
   begin
     if (write_addr < CONFIG_SIZE)
         begin
@@ -177,7 +177,7 @@ end
 // reading operands and result
 ///////////////////////////////////////////////////////////////////////////
 logic [$clog2(CONFIG_SIZE + 2) - 1:0] read_addr;
-logic read_mask_enable, read_enable;
+logic read_mask_disable, read_enable;
 assign axil_arready = 1;
 
 always_ff @ (posedge clk)
@@ -187,14 +187,14 @@ begin
   read_enable <= axil_arvalid;
     if(axil_arvalid)
     begin
-        read_mask_enable <= axil_awaddr > START_OFFSET & (axil_awaddr < (START_OFFSET + ((CONFIG_SIZE + 2) <<2)));
+        read_mask_disable <= ~(axil_awaddr > START_OFFSET & (axil_awaddr < (START_OFFSET + ((CONFIG_SIZE + 2) <<2))));
         read_addr <= axil_araddr[2+:$clog2(CONFIG_SIZE + 2)];
     end
 end
 else
 begin
   read_enable <= 0;
-  read_mask_enable <= 0;
+  read_mask_disable <= 0;
   read_addr <= 0;
 end
 end
@@ -209,8 +209,8 @@ if(resetn)
 begin
   if (read_enable)
   begin
-     axil_rvalid <= ~read_mask_enable;
-     if (read_mask_enable)
+     axil_rvalid <= ~read_mask_disable;
+     if (read_mask_disable)
       axil_rdata <= 0;
      else
      begin
