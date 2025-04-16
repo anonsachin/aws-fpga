2ce398463de60ba5ba6de3f5f88daee26c18f278
anonsachin <sachin94roxz@gmail.com>
2025-04-16 11:48:28 +0530

    added extra display information

--- hdk/cl/examples/cl_agent/verif/tests/cl_agent_config_test.sv
+++ hdk/cl/examples/cl_agent/verif/tests/cl_agent_config_test.sv
@@ -103,8 +103,8 @@ module cl_agent_config_test();
 
     for (int i=0; i<5; i++) begin
       $display("The active wire of the target[%0d] is %0d", i, targets.row[i].position);
-      $display("The active wire of the target[%0d] is %0d", i, controls.row[i][0].position);
-      $display("The active wire of the target[%0d] is %0d", i, controls.row[i][1].position);
+      $display("The active wire of the control[%0d][0] is %0d", i, controls.row[i][0].position);
+      $display("The active wire of the control[%0d][1] is %0d", i, controls.row[i][1].position);
         store_write.configValue.configValue.shuffleWires[0][i] = targets.row[i];
         store_write.configValue.configValue.shuffleWires[1][i] = controls.row[i][0];
         store_write.configValue.configValue.shuffleWires[2][i] = controls.row[i][1];
